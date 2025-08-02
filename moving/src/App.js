77b5c917390f828587428f873f116967f4378b4a
egs125 <egs125@gmail.com>
2025-08-02 13:33:28 +0900

    first commit

---
+++ moving/src/App.js
@@ -0,0 +1,12 @@
+import Container from '@mui/material/Container';
+import List from "./List";
+
+function App() {
+  return (
+    <Container maxWidth="xl">
+      <List />
+    </Container>
+  );
+}
+
+export default App;
